50dbd5ee4aa6464bf37c50695ed8219e3833914e
H Lowe <haleylowe0@gmail.com>
2020-10-02 17:50:16 -0700

    created seeds and schema files in db folder

---
+++ Develop/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('california burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('protein style burger', false);
