fb6f0dea74b49b6b280522aebec447cb2724ea8b
H Lowe <haleylowe0@gmail.com>
2020-10-25 22:41:14 -0700

    changed connection.js createCOnnection function

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,25 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+
+        // 3306
+        port: 3306,
+
+        // Your username
+        user: process.env.DATABASE_USERNAME,
+
+        // Your password
+        password: process.env.DATABASE_PASSWORD,
+        database: "burgers_db"
+    });
+}
 
 // Make connection.
 connection.connect(function(err) {
