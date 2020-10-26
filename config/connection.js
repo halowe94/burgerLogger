33c0fe49fa49b61138c859fbb22f87a6966c0587
H Lowe <haleylowe0@gmail.com>
2020-10-25 21:29:28 -0700

    added JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: "",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 // Make connection.
 connection.connect(function(err) {
